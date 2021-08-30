86412cbf4df4db080be23dde4b024a0559ed04c6
Luigi Sacco <luigi.sa94@gmail.com>
2021-08-30 17:23:51 +0200

    All steps completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,88 +1,85 @@
 
+// Global variables
+var modal = document.getElementById("rulesModal");
+var closeBtn = document.getElementById("closeBtn");
 const defaultMin = 1, defaultMax = 200
-var inputMin = defaultMin, inputMax= defaultMax, error = 0
+var min = defaultMin, max= defaultMax, error = 0
 
+// Confirm the range
 function confirm() {
-    let minValue = document.getElementById("inputMin").value
-    let maxValue = document.getElementById("inputMax").value
+    let labelMin = document.getElementById("labelMin")
+    let labelMax = document.getElementById("labelMax")
+    let inputMin = document.getElementById("inputMin")
+    let inputMax = document.getElementById("inputMax")
 
-    inputMin = parseInt((minValue) ? minValue : defaultMin)
-    inputMax = parseInt((maxValue) ? maxValue : defaultMax)
+    let minValue = inputMin.value
+    let maxValue = inputMax.value
 
-    if (inputMin < defaultMin || inputMin > (defaultMax/2)) {
-        document.getElementById("labelMin").innerHTML = "Insert a number in range 1 to 100!"
-        inputMin = defaultMin
+    min = parseInt((minValue) ? minValue : defaultMin)
+    max = parseInt((maxValue) ? maxValue : defaultMax)
+
+    if (min < defaultMin || min > (defaultMax/2)) {
+        labelMin.innerHTML = "Insert a number in range 1 to 100!"
+        min = defaultMin
         error = 2
     } 
-    else document.getElementById("labelMin").innerHTML = ""
+    else labelMin.innerHTML = ""
 
-    if (inputMax <= defaultMin || inputMax > defaultMax) {
-        document.getElementById("labelMax").innerHTML = "Insert a number in range 2 to 200!"
-        inputMax = defaultMax
+    if (max <= defaultMin || max > defaultMax) {
+        labelMax.innerHTML = "Insert a number in range 2 to 200!"
+        max = defaultMax
         error = 2
     } 
-    else document.getElementById("labelMax").innerHTML = ""
+    else labelMax.innerHTML = ""
 
-    if (inputMax <= inputMin) {
+    if (max <= min) {
         error = 1
-        inputMax = defaultMax
+        max = defaultMax
     }
 
     if (error == 0)
     {
-        document.getElementById("labelMin").innerHTML = ""
-        document.getElementById("labelMax").innerHTML = ""
+        labelMin.innerHTML = ""
+        labelMax.innerHTML = ""
     } 
     else if (error == 1) {
-        document.getElementById("labelMax").innerHTML = "Max number must be greater than Min!<br>If you start now it will be automatically set at 200"
+        labelMax.innerHTML = "Max number must be greater than Min!<br>If you start now it will be automatically set at 200"
     }
 
-    document.getElementById("confirmed").innerHTML = "The range is " + inputMin + " to " + inputMax + "<br>You can now start or change the range again!"
+    document.getElementById("confirmed").innerHTML = "The range is " + min + " to " + max + "<br>You can now start or change the range again!"
     error = 0;
 }
 
+// Start the FizzBuzz game
 function start(opt) {
-    document.getElementById("shell-screen").innerHTML = ""
-    let elseFlag = 0
-    
 
-    for (let i = inputMin; i <= inputMax; i++)
+    let shell = document.getElementById("shell-screen")
+
+    shell.innerHTML = "" // clean the shell
+
+    for (let i = min; i <= max; i++)
     {
-        if (i % 3 == 0) {
-            document.getElementById("shell-screen").innerHTML += (opt == 1) 
+        if (i % 3 == 0 && i % 5 == 0) {
+            shell.innerHTML += "<div class='shell-text'>" + smallBig(i) + "FizzBuzz FTW" + "</div>"
+        } else if (i % 3 == 0) {
+            // if opt is 2 fizz & buzz are inverted
+            shell.innerHTML += (opt == 1) 
                 ? "<div class='shell-text'>" + smallBig(i) + "Fizz GG" + "</div>"
                 : "<div class='shell-text'>" + smallBig(i) + "Buzz GG" + "</div>"
-            elseFlag = 1
-        }
-        else if (i % 5 == 0) {
-            document.getElementById("shell-screen").innerHTML += (opt == 1) 
+        } else if (i % 5 == 0) {
+            // if opt is 2 fizz & buzz are inverted
+            shell.innerHTML += (opt == 1) 
                 ? "<div class='shell-text'>" + smallBig(i) + "Buzz GG" + "</div>"
                 : "<div class='shell-text'>" + smallBig(i) + "Fizz GG" + "</div>"
-            elseFlag = 1
+        } else if (i % 7 == 0 && i % 11 == 0) {
+            shell.innerHTML += "<div class='shell-text'>" + smallBig(i) + "FooBoo" + "</div>"
+        } else if (i % 7 == 0) {
+            shell.innerHTML += "<div class='shell-text'>" + smallBig(i) + "Foo" + "</div>"
+        } else if (i % 11 == 0) {
+            shell.innerHTML += "<div class='shell-text'>" + smallBig(i) + "Boo" + "</div>"
+        } else {
+            shell.innerHTML += "<div class='shell-text'>" + smallBig(i) + (i + "</div>")
         }
-        else if (i % 7 == 0) {
-            document.getElementById("shell-screen").innerHTML += "<div class='shell-text'>" + smallBig(i) + "Foo" + "</div>"
-            elseFlag = 1
-        }
-        else if (i % 11 == 0) {
-            document.getElementById("shell-screen").innerHTML += "<div class='shell-text'>" + smallBig(i) + "Boo" + "</div>"
-            elseFlag = 1
-        }
-
-        if (i % 3 == 0 && i % 5 == 0) {
-            document.getElementById("shell-screen").innerHTML += "<div class='shell-text'>" + smallBig(i) + "FizzBuzz FTW" + "</div>"
-            elseFlag = 1
-        }
-
-        if (i % 7 == 0 && i % 11 == 0) {
-            document.getElementById("shell-screen").innerHTML += "<div class='shell-text'>" + smallBig(i) + "FooBoo" + "</div>"
-            elseFlag = 1
-        }
-
-        if (elseFlag == 0) {
-            document.getElementById("shell-screen").innerHTML += "<div class='shell-text'>" + smallBig(i) + (i + "</div>")
-        }
-        elseFlag = 0
     }
 }
 
@@ -91,3 +88,23 @@ function smallBig(i) {
     else if (i > 95) return "(Big) "
     else return ""
 }
+
+// Modal start
+
+// Variables reminder
+// modal = document.getElementById("rulesModal");  
+// closeBtn = document.getElementById("closeBtn");
+
+function showRules() {
+    modal.style.display = "block";
+}
+function closeRules() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// Modal end
